533ff5874bf9afd9ea9f6537ad3163fbb44b96df
Christopher Dutcher <cadutcher@icloud.com>
2020-06-22 16:14:53 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,8 @@ if array.length == 1
   array.join
 elsif array.length <= 2
   array.join(" and ")
-elsif array.length <= 3
-  array.
+elsif array.length > 2
+   array[-1].prepend "and "
+ array.join(", ")
 end
 end
